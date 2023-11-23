83f5700b4f0c758a645e6f140d743449180fa013
Tyson Burton <tysonburton123@gmail.com>
2023-11-23 18:26:04 -0500

    Connected to Database
    -Fixed Connection to database
    - Crud Operation still need to be fixed.

--- config/config.js
+++ config/config.js
@@ -1,17 +1,12 @@
-// require("dotenv").config();
-import dotenv from 'dotenv';
-dotenv.config();
 const config = {
-    env: process.env.NODE_ENV || 'development', 
+    env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI ||
-    "mongodb+srv://admin:admin@cluster0.hqb8bfm.mongodb.net/" ||
-    process.env.MONGO_HOST ||    
-    'mongodb://' + (process.env.IP || 'localhost') + ':' + 
-   (process.env.MONGO_PORT || '27017') +
-    '/BloggingPlatform' 
-}
-export default config
-// module.exports = config;
-   
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://admin:admin@cluster0.hqb8bfm.mongodb.net/" ||
+      process.env.MONGO_HOST ||
+      'mongodb://' + (process.env.IP || 'localhost') + ':' +
+      (process.env.MONGO_PORT || '27017') +
+      '/Blogging_Database'
+  };
+  
+  export default config;
