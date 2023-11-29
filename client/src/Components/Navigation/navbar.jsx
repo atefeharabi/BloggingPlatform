4bfea5670ca47f0381d12b8275497789e9532db0
Tyson <tysonburton123@gmail.com>
2023-11-29 00:42:12 -0500

    Portals/Enemy fix

--- client/src/Components/Navigation/navbar.jsx
+++ client/src/Components/Navigation/navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom"
-export default function Navbar(){
+import teamLogo from './logo.png';
+export default function Navbar()
+{
     // const path = window.location.pathname
     return <nav className="nav">
-        <img src="logo.png" alt="Logo"/> 
+        <img src={teamLogo} style={{ width: '100px', height: '100px' }} alt="Team Logo" />
         <ul>
             <li><Link to="/">Blogging Platform</Link></li>
             <li><Link to="/posts">Posts</Link></li>
